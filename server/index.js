5422cf4df717c5f54dde0ddcfefdfed3ed3f323e
Edgars <edgars.avenins@gmail.com>
2020-04-30 17:38:50 +1200

    app setup

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const envConfig = require('dotenv').config()
+if(envConfig.error) throw envConfig.error
+
+const server = require('./server')
+const port = process.env.PORT || 3000
+
+server.listen(port, function () {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port', port)
+})
