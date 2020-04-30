d1dd32f90d4b23417832439ab7cb99a50e1a83f7
Edgars Avenins <eave1991@gmail.com>
2020-04-30 17:47:01 +1200

    Update config.js

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
  export const firebaseConfig = {
-    //apiKey: "$$$AIzaSyDMDiPS8_uZ0pALN1qCQMf3z5UI2WyqVyA",
+    //apiKey: "$$$AIzaSyDMDiPS8_$$$uZ0pALN1qCQMf3z5UI2WyqVyA",
     authDomain: "workout-time-f79a6.firebaseapp.com",
     databaseURL: "https://workout-time-f79a6.firebaseio.com",
     projectId: "workout-time-f79a6",
